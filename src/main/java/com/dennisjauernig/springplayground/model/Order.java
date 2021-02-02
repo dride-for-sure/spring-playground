83b041efc2779ac67aecce80a1c9a6f518da0cf2
dennisjauernig <dennis@jauernig.email>
2021-02-02 16:10:27 +0100

    ReAdd Lombock

--- src/main/java/com/dennisjauernig/springplayground/model/Order.java
+++ src/main/java/com/dennisjauernig/springplayground/model/Order.java
@@ -1,9 +1,11 @@
 package com.dennisjauernig.springplayground.model;
 
+import lombok.Data;
+
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.UUID;
 
+@Data
 public class Order {
 
  private UUID id = UUID.randomUUID();
@@ -17,41 +19,5 @@ public class Order {
  public Order () {
  }
 
- public UUID getId () {
-	return this.id;
- }
-
- public void setId (UUID id) {
-	this.id = id;
- }
-
- public ArrayList<String> getProductIds () {
-	return this.productIds;
- }
-
- public void setProductIds (ArrayList<String> productIds) {
-	this.productIds = productIds;
- }
-
- @Override
- public String toString () {
-	return "Order{" +
-					"id=" + this.id +
-					", productIds=" + this.productIds +
-					'}';
- }
-
- @Override
- public boolean equals (Object o) {
-	if ( this == o ) return true;
-	if ( o == null || this.getClass() != o.getClass() ) return false;
-	Order order = (Order) o;
-	return Objects.equals( this.getId(), order.getId() ) && Objects.equals( this.getProductIds(), order.getProductIds() );
- }
-
- @Override
- public int hashCode () {
-	return Objects.hash( this.getId(), this.getProductIds() );
- }
 }
 
--- src/main/java/com/dennisjauernig/springplayground/model/Product.java
+++ src/main/java/com/dennisjauernig/springplayground/model/Product.java
@@ -1,7 +1,8 @@
 package com.dennisjauernig.springplayground.model;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class Product {
 
  private String id;
@@ -15,40 +16,4 @@ public class Product {
  public Product () {
  }
 
- public String getId () {
-	return this.id;
- }
-
- public void setId (String id) {
-	this.id = id;
- }
-
- public String getName () {
-	return this.name;
- }
-
- public void setName (String name) {
-	this.name = name;
- }
-
- @Override
- public String toString () {
-	return "Product{" +
-					"id='" + this.id + '\'' +
-					", name='" + this.name + '\'' +
-					'}';
- }
-
- @Override
- public boolean equals (Object o) {
-	if ( this == o ) return true;
-	if ( o == null || this.getClass() != o.getClass() ) return false;
-	Product product = (Product) o;
-	return Objects.equals( this.id, product.id ) && Objects.equals( this.name, product.name );
- }
-
- @Override
- public int hashCode () {
-	return Objects.hash( this.id, this.name );
- }
 }
